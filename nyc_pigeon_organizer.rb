cd528ea07c6dda72dacb8aff920bbbd807c9b329
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-03 23:23:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
+require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
+  dataList = {}
+  data.each do |cgl, cglVal|
+    cglVal.each do |cglData, pig|
+      pig.each do |ele|
+        dataList[ele] = {}
+      end
+    end
+  end
+  data.each do |cgl, cglVal|
+    cglVal.each do |cglData, pig|
+      pig.each do |ele|
+        dataList[ele][cgl] = []
+      end
+    end
+  end
+  #List, pigKey = data, aryEle
+  #List, cglKey = data, cglKey
+  #List, cglVal = data, aryEleKey
+  data.each do |dataCgl, dataCglVal|
+    dataCglVal.each do |dataCglKey, dataPig|
+      dataPig.each do |value|
+        dataList[value][dataCgl] << dataCglKey.to_s
+      end
+    end
+  end
+  return dataList
 end
